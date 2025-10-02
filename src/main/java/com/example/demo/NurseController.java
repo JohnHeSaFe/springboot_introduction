f631d9b62caa9cfce7b507277d55eee1175b0b0c
JohnHeSaFe <henardsalango09@gmail.com>
2025-10-02 16:18:01 +0200

    fixed login endpoint

--- src/main/java/com/example/demo/NurseController.java
+++ src/main/java/com/example/demo/NurseController.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,17 +18,24 @@ public class NurseController {
     JSONArray nurses = getAll();
 
     @PostMapping("/login")
-    public boolean login(@RequestParam(value="name") String name, @RequestParam(value="password") String password) {
+    public ResponseEntity<Map<String, Boolean>> login(@RequestBody Map<String, String> body) {
+        String name = body.get("name");
+        String password = body.get("password");
+        boolean answer = false;
         for (int i = 0; i < nurses.length(); i++) {
             JSONObject nurse = nurses.getJSONObject(i);
             if (nurse.getString("name").equals(name) && nurse.getString("password").equals(password)) {
-                return true;
+                answer = true;
+                break;
             }
         }
 
-        return false;
-    }
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("response", answer);
 
+        return ResponseEntity.ok(response);
+    }
+    
     private JSONArray getAll() {
         return new JSONArray();
     }
